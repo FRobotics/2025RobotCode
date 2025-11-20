17bffa69b1f49ad502712a2d33b80c2efde56f7a
FRobotics4150 <33380251+FRobotics4150@users.noreply.github.com>
2025-11-19 20:50:16 -0500

    framework for autos

--- JavaSwerveDrive2/Java Swerve Drive/src/main/java/frc/robot/SwerevOdometry.java
+++ JavaSwerveDrive2/Java Swerve Drive/src/main/java/frc/robot/SwerevOdometry.java
@@ -1,5 +1,34 @@
 package frc.robot;
 
 public class SwerevOdometry {
+
+    static void init(){
+
+    }
     
+    static void execute(){}
+
+    static double getExecElapsedTimeMillii(){
+        return(0.0);
+    }
+
+    static void getpose(){
+      
+    }
+
+    static double getrotposition(){
+        return(0.0);
+    }
+
+    static double getxposition(){
+        return(0.0);
+    }
+
+    static double getyposition(){
+        return(0.0);
+    }
+
+    static void setstartingpose(){}
+
 }
+

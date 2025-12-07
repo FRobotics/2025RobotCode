2475add3db36ba9eb91f5882f0ed23e6c3c56001
jsimpso81 <60305400+jsimpso81@users.noreply.github.com>
2025-12-07 16:06:01 -0500

    Added swerve drive control code

--- JavaSwerveDrive2/Java Swerve Drive/src/main/java/frc/robot/swervemodule.java
+++ JavaSwerveDrive2/Java Swerve Drive/src/main/java/frc/robot/swervemodule.java
@@ -2,8 +2,17 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import Lib4150.Lib4150PositionControl;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import com.ctre.phoenix6.hardware.CANcoder;
+
 
 public class swervemodule {
 
@@ -17,11 +26,14 @@ public class swervemodule {
     private Translation2d moduleoffset;
     private double spinPosRad = 0.0;
     private double driveSpeedMPS = 0.0;
-    private PWMTalonSRX speedMotor;
-    private PWMTalonSRX rotationMotor;
+    private SparkMax driveMotor;
+    private SparkMax spinMotor;
+    private Lib4150PositionControl spinPositionControl;
+    private PIDController drivePID;
+    private SimpleMotorFeedforward drivFeedforward;
+    private CANcoder absoluteAngle;
 
-
-    public swervemodule(int speedID, int rotID, double paraXoffset, double paraYoffset, int paradriveid, int paraspinid, int paraspinEnc, double paramagOffset) {
+    public swervemodule(double paraXoffset, double paraYoffset, int paradriveid, int paraspinid, int paraspinEnc, double paramagOffset) {
         xOff = paraXoffset;
         yOff = paraYoffset;
         driveid = paradriveid;
@@ -30,8 +42,15 @@ public class swervemodule {
         magOffset = paramagOffset;
 
         moduleoffset = new Translation2d(xOff, yOff);
-        speedMotor = new PWMTalonSRX(speedID);
-        rotationMotor = new PWMTalonSRX(rotID);
+        driveMotor = new SparkMax(driveid,MotorType.kBrushless);
+        spinMotor = new SparkMax(spinid,MotorType.kBrushless);
+        absoluteAngle = new CANcoder(spinEnc);
+        
+        // (Error Deadband,error threshhold )
+        spinPositionControl = new Lib4150PositionControl(Units.degreesToRadians(2.0), Units.degreesToRadians(50.0), 0.005, 0.35, 0.8, 1.0e-5, false, true);
+
+        drivFeedforward = new SimpleMotorFeedforward (0.0, 0.07645406, 0.0);
+        drivePID = new PIDController (0.07645406*1.0, 0.07645406*0.5, 1.0e-7*0.7645406);
 
     }
 
@@ -40,13 +59,10 @@ public class swervemodule {
     }
         
     public void ExecuteLogic( SwerveModuleState parmModState, double timeValue ) {
-        double Angle = parmModState.angle.getRadians();
-        double Speed = parmModState.speedMetersPerSecond;
         
-        speedMotor.set(Speed);
         //TODO:
         //Check how encoders actualy output; maybe need to change this.
-        if(spinPosRad > Angle)
+        /*/if(spinPosRad > Angle)
         {
             rotationMotor.set(0.1); //current pos greater than desired > cw
         }
@@ -57,7 +73,21 @@ public class swervemodule {
         else
         {
             rotationMotor.set(0.0); //at right angle; stop
-        }
+        }*/
+        double currentAngle = absoluteAngle.getAbsolutePosition().getValueAsDouble()*2.0*Math.PI;
+        parmModState.optimize(new Rotation2d(currentAngle));
+        
+        double targetAngle = parmModState.angle.getRadians();
+
+        spinMotor.set(spinPositionControl.PosCtrlExec(targetAngle, currentAngle));
+        
+
+        double TargetSpeed = parmModState.speedMetersPerSecond;
+        double ActualSpeed = driveMotor.getEncoder().getVelocity();
+        double feedForward = drivFeedforward.calculate(TargetSpeed);
+        double PIDoutput = drivePID.calculate(ActualSpeed, TargetSpeed);
+        double MotorDemand = MathUtil.clamp(feedForward+PIDoutput, -1.0, 1.0);
+        driveMotor.set(MotorDemand);
 
     }
 
